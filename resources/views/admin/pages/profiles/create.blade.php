f8597c61f7bb44b9967baf6bfacceafcc7eb380a
Dhaka Prokash Limited <dhakaprokash22@gmail.com>
2025-05-18 15:46:47 +0600

    Some Modification

--- resources/views/admin/pages/profiles/create.blade.php
+++ resources/views/admin/pages/profiles/create.blade.php
@@ -5,7 +5,6 @@ Create Profile
 @push('admin_css')
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/css/toastr.min.css">
-<link href="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-bs4.min.css" rel="stylesheet">
 @endpush
 
 @section('admin_content')
@@ -31,7 +30,7 @@ Create Profile
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleInputEmail3">Description</label>
-                                            <textarea name="description" class="form-control summernote" placeholder="Description" required></textarea>
+                                            <textarea name="description" class="form-control textarea" placeholder="Description" required></textarea>
                                         </div>
                                         <div class="form-group">
                                             <label for="exampleInputName1">File Type</label>
@@ -71,28 +70,10 @@ Create Profile
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/summernote@0.9.0/dist/summernote-bs4.min.js"></script>
+    <script src="{{ asset('/storage/admin/assets/tinymce/tinymce.min.js') }}"></script>
     <script src="{{ asset('/storage/admin/assets/js/file-upload.js') }}"></script>
     <script>
         $(document).ready(function() {
-            $('.summernote').summernote({
-                placeholder: 'Type Here...',
-                tabsize: 2,
-                height: 150,
-                toolbar: [
-                    // Basic style options
-                    ['style', ['bold', 'italic', 'underline', 'clear']],
-                    ['font', ['fontname', 'fontsize', 'strikethrough', 'superscript', 'subscript']],
-                    ['para', ['ul', 'ol', 'paragraph', 'height', 'style']],
-                    ['align', ['left', 'center', 'right', 'justify']],
-                    ['insert', ['link', 'picture', 'video']],
-                    ['view', ['fullscreen', 'codeview', 'help']],
-                    ['color', ['forecolor', 'backcolor']],
-                    ['table', ['table']],
-                    ['misc', ['undo', 'redo']]
-                ]
-            });
-
             $('button[id^="profileDeleteButton-"]').click(function() {
                 var profileId = $(this).attr('id').split('-')[1];
                 Swal.fire({
@@ -119,6 +100,68 @@ Create Profile
                 }
             });
 
+        });
+
+
+
+
+        tinymce.init({
+        selector: '.textarea',
+        plugins: 'advlist autolink lists textcolor colorpicker link code wordcount searchreplace charmap anchor visualblocks fullscreen table contextmenu paste',
+        menubar: true,
+        height: "200",
+        toolbar1: 'insertfile | bold italic underline fontsizeselect fontselect | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | forecolor backcolor link code',
+        fontsize_formats: "1pt 2pt 3pt 4pt 5pt 6pt 7pt 8pt 9pt 10pt 11pt 12pt 13pt 14pt 15pt 16pt 17pt 18pt 19pt 20pt 21pt 22pt 23pt 24pt 25pt 26pt 27pt 28pt 29pt 30pt 31pt 32pt 33pt 34pt 35pt 36pt 37pt 38pt 39pt 40pt 41pt 42pt 43pt 44pt 45pt 46pt 47pt 48pt 49pt 50pt",
+        font_formats:
+            "Arial=arial,helvetica,sans-serif;" +
+            "Arial Black=arial black,avant garde;" +
+            "Comic Sans MS=comic sans ms,sans-serif;" +
+            "Courier New=courier new,courier,monospace;" +
+            "Georgia=georgia,palatino;" +
+            "Impact=impact,chicago;" +
+            "Lucida Console=lucida console,monaco,monospace;" +
+            "Lucida Sans Unicode=lucida sans unicode,lucida grande,sans-serif;" +
+            "Palatino Linotype=palatino linotype,book antiqua,palatino,serif;" +
+            "Segoe UI=segoe ui,sans-serif;" +
+            "Tahoma=tahoma,arial,helvetica,sans-serif;" +
+            "Times New Roman=times new roman,times,serif;" +
+            "Trebuchet MS=trebuchet ms,geneva,sans-serif;" +
+            "Verdana=verdana,geneva,sans-serif;" +
+            "Helvetica=helvetica;" +
+            "Gill Sans=gill sans,gill sans mt,calibri,sans-serif;" +
+            "Calibri=calibri,candara,Segoe,Segoe UI,Optima,Arial,sans-serif;" +
+            "Cambria=cambria,georgia,serif;" +
+            "Garamond=garamond,baskerville,serif;" +
+            "Bookman=bookman,serif;" +
+            "Avant Garde=avant garde,sans-serif;" +
+            "Century Gothic=century gothic,apple gothic,sans-serif;" +
+            "Franklin Gothic Medium=franklin gothic medium,arial black,sans-serif;" +
+            "Candara=candara,calibri,Segoe,Segoe UI,Optima,Arial,sans-serif;" +
+            "Optima=optima,segoe,segoe ui,candara,calibri,arial,sans-serif;" +
+            "Futura=futura,sans-serif;" +
+            "Rockwell=rockwell,serif;" +
+            "Baskerville=baskerville,serif;" +
+            "Didot=didot,serif;" +
+            "Perpetua=perpetua,serif;" +
+            "Big Caslon=big caslon,serif;" +
+            "Consolas=consolas,monaco,monospace;" +
+            "Monaco=monaco,consolas,monospace;" +
+            "Ubuntu=ubuntu,sans-serif;" +
+            "Roboto=roboto,arial,sans-serif;" +
+            "Open Sans=open sans,arial,sans-serif;" +
+            "Lato=lato,arial,sans-serif;" +
+            "Raleway=raleway,arial,sans-serif;" +
+            "PT Sans=pt sans,arial,sans-serif;" +
+            "Noto Sans=noto sans,arial,sans-serif;" +
+            "Noto Serif=noto serif,serif;",
+        rel_list: [
+            { title: 'nofollow', value: 'nofollow' },
+            { title: 'follow', value: 'follow' }
+        ],
+        media_live_embeds: false,
+        relative_urls: false,
+        remove_script_host: false,
+        extended_valid_elements: 'script[type|src|charset]'
         });
     </script>
 @endpush
--- resources/views/frontend/pages/contactus.blade.php
+++ resources/views/frontend/pages/contactus.blade.php
@@ -61,39 +61,88 @@
                 <form action="{{ route('home.contact') }}" method="post">
                     @csrf
                     <div class="row">
+                        <!-- Name -->
                         <div class="col-lg-4">
                             <div class="form-group">
                                 <label>Name</label>
-                                <input id="name" name="name" type="text" class="no-margin-bottom">
+                                <input
+                                    id="name"
+                                    name="name"
+                                    type="text"
+                                    class="form-control no-margin-bottom @error('name') is-invalid @enderror"
+                                    value="{{ old('name') }}">
+                                @error('name')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
+
+                        <!-- Email -->
                         <div class="col-lg-4">
                             <div class="form-group">
                                 <label>Email</label>
-                                <input id="email" type="email" name="email" class="no-margin-bottom">
+                                <input
+                                    id="email"
+                                    type="email"
+                                    name="email"
+                                    class="form-control no-margin-bottom @error('email') is-invalid @enderror"
+                                    value="{{ old('email') }}">
+                                @error('email')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
+
+                        <!-- Phone -->
                         <div class="col-lg-4">
                             <div class="form-group">
                                 <label>Phone</label>
-                                <input id="phone" type="number" name="phone" class="no-margin-bottom">
+                                <input
+                                    id="phone"
+                                    type="number"
+                                    name="phone"
+                                    class="form-control no-margin-bottom @error('phone') is-invalid @enderror"
+                                    value="{{ old('phone') }}">
+                                @error('phone')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
+
+                        <!-- Subject -->
                         <div class="col-lg-12">
                             <div class="form-group">
                                 <label>Subject</label>
-                                <input id="subject" type="text" name="subject" class="no-margin-bottom">
+                                <input
+                                    id="subject"
+                                    type="text"
+                                    name="subject"
+                                    class="form-control no-margin-bottom @error('subject') is-invalid @enderror"
+                                    value="{{ old('subject') }}">
+                                @error('subject')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
+
+                        <!-- Message -->
                         <div class="col-lg-12 margin-20px-bottom sm-margin-10px-bottom">
                             <div class="form-group">
                                 <label>Message</label>
-                                <textarea id="text" name="message" class="form-control line-height-70"></textarea>
+                                <textarea
+                                    id="text"
+                                    name="message"
+                                    class="form-control line-height-70 @error('message') is-invalid @enderror">{{ old('message') }}</textarea>
+                                @error('message')
+                                    <div class="text-danger">{{ $message }}</div>
+                                @enderror
                             </div>
                         </div>
+
+                        <!-- Submit -->
                         <div class="col-lg-12">
                             <div class="Send">
-                                <button type="submit" href="javascript:void(0)" class="butn">Send Message</button>
+                                <button type="submit" class="butn">Send Message</button>
                             </div>
                         </div>
                     </div>
